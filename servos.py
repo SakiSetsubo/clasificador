aef40623c611098d1708014a1dc1b8bfe5ca42eb
Pablo Esteban <sakisetsubo@gmail.com>
2024-08-17 21:36:04 -0500

    456asdasdfasdffasdfasdfasdfasdfasdfasdfasdfasdfasdf

--- servos.py
+++ servos.py
@@ -8,22 +8,22 @@ SERVO_BANDA_PIN = 25   # Cambia estos pines a los que estás usando
 
 # Configuración de los servos
 SERVO_MADURO_PWM_FREQ = 50
-#SERVO_DANADO_PWM_FREQ = 50
+SERVO_DANADO_PWM_FREQ = 50
 SERVO_BANDA_PWM_FREQ = 50
 
 # Inicialización de GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SERVO_MADURO_PIN, GPIO.OUT)
-#GPIO.setup(SERVO_DANADO_PIN, GPIO.OUT)
+GPIO.setup(SERVO_DANADO_PIN, GPIO.OUT)
 GPIO.setup(SERVO_BANDA_PIN, GPIO.OUT)
 
 # Configuración de PWM para cada servo
 servo_maduro_pwm = GPIO.PWM(SERVO_MADURO_PIN, SERVO_MADURO_PWM_FREQ)
-#servo_danado_pwm = GPIO.PWM(SERVO_DANADO_PIN, SERVO_DANADO_PWM_FREQ)
+servo_danado_pwm = GPIO.PWM(SERVO_DANADO_PIN, SERVO_DANADO_PWM_FREQ)
 servo_banda_pwm = GPIO.PWM(SERVO_BANDA_PIN, SERVO_BANDA_PWM_FREQ)
 
 servo_maduro_pwm.start(0)
-#servo_danado_pwm.start(0)
+servo_danado_pwm.start(0)
 servo_banda_pwm.start(0)
 
 
